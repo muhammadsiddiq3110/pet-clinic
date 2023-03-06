1157c777c512319d37c89937a968d2c661c3a849
muhammadsiddiq <jdjdjdjdjdjjdj9@gmail.com>
2023-03-06 11:08:10 +0500

    Implement Map Based services . Closed #5

---
+++ pet-clinic-data/src/main/java/uz/fargona/service/map/OwnerServiceMap.java
@@ -0,0 +1,39 @@
+package uz.fargona.service.map;
+
+import lombok.Lombok;
+import uz.fargona.model.Owner;
+import uz.fargona.service.OwnerService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long > implements OwnerService {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+    super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
+    }
+}
---
+++ pet-clinic-data/src/main/java/uz/fargona/service/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package uz.fargona.service.map;
+
+import uz.fargona.model.Pet;
+import uz.fargona.service.PetService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet,Long> implements PetService {
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+    super.deleteById(id);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return null;
+    }
+}
