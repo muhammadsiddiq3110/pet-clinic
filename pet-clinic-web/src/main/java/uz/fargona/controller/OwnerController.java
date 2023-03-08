b7ea9909a22a3267fc412f5df212e294b5bd9e4c
muhammadsiddiq <jdjdjdjdjdjjdj9@gmail.com>
2023-03-08 15:30:11 +0500

    Added list of Owners to the page. Closed #10

--- pet-clinic-web/src/main/java/uz/fargona/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/uz/fargona/controller/OwnerController.java
@@ -1,14 +1,26 @@
 package uz.fargona.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import uz.fargona.service.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
-@GetMapping({"","/","index","index.html"})
-public String listOwners(){
-    return "owner/index";
-}
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @GetMapping({"", "/", "index", "index.html"})
+    public String listOwners(Model model) {
+        var list=ownerService.findAll().stream()
+                        .sorted((a,b)->(a.getId()>b.getId())?1:0);
+        model.addAttribute("owners",list);
+        return "owner/index";
+    }
 }
