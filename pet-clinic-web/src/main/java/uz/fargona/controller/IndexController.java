c95a999d5767eeda5c0c784cbe8a9e9cd32b0265
muhammadsiddiq <jdjdjdjdjdjjdj9@gmail.com>
2023-03-11 12:12:42 +0500

    Static resources are added Closed #14

--- pet-clinic-web/src/main/java/uz/fargona/controller/IndexController.java
+++ pet-clinic-web/src/main/java/uz/fargona/controller/IndexController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-    @RequestMapping({"","/,","index","1.html"})
-    public String index(){
+
+    @RequestMapping({"", "/", "/index", "index.html"})
+    public String index() {
         return "index";
     }
+
 }
