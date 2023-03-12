d22d647184aa30611e28ac31c35842b57af984bd
muhammadsiddiq <jdjdjdjdjdjjdj9@gmail.com>
2023-03-12 21:32:28 +0500

    Added Contact info to Owner. Closed #21

--- pet-clinic-data/src/main/java/uz/fargona/model/Owner.java
+++ pet-clinic-data/src/main/java/uz/fargona/model/Owner.java
@@ -6,4 +6,27 @@ public class Owner extends Person{
     private String city;
     private String telephone;
 
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 }
