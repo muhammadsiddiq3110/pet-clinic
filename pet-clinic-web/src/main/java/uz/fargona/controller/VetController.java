432d1fa3aca364e3601f108bd3d22cfbc2ef6b0a
muhammadsiddiq <jdjdjdjdjdjjdj9@gmail.com>
2023-03-30 15:12:07 +0500

    Vets page is improved. Closed #32

--- pet-clinic-web/src/main/java/uz/fargona/controller/VetController.java
+++ pet-clinic-web/src/main/java/uz/fargona/controller/VetController.java
@@ -2,8 +2,13 @@ package uz.fargona.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import uz.fargona.model.Vet;
 import uz.fargona.service.VetService;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("/vets")
@@ -21,5 +26,9 @@ public class VetController {
                 .toList();
         model.addAttribute("vets", list);
         return "vet/index";
+    }
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetJson(){
+        return vetService.findAll();
     }
 }
